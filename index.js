b2c135462f5e8b7c9fd544960f96e02d2f42c5b2
camtaylor <camerontaylor00@gmail.com>
2016-10-03 23:38:34 -0700

    part 2

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ var fs = require('fs');
 var http = require('http')
 
 
+
 http.createServer(function(req, res){
-        res.writeHead(  200,  {  'Content-Type' : 'text/html'  } );
-        res.write(fs.readFileSync('public/index.html', 'utf-8'));
-        res.end(); 
+        fs.stat('public/index.html', function(err, success){
+        if (err==null) {
+
+        }  else if ( err.code == 'ENOENT'){
+            console.error('File does not exist'); 
+        }
+        else {
+            console.error(err);
+        }        
+        fs.open("public/index.html", 'r', function(error, data) {
+            var buffer = new Buffer(4096);
+            fs.read(data, buffer, 0, buffer.length, 0, function(err, bytes) {
+                var textData = buffer.slice(0, bytes).toString('ascii');
+                res.writeHead(  200,  {  'Content-Type' : 'text/html'  } );
+                res.write(textData);
+                res.end();
+                fs.close(data, function(error) {
+                    if (err) { throw error; }
+                });
+            });
+        });
+        
+        });
 
 }).listen(8080);
