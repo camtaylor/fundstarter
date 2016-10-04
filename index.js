4e09f7effb188accd7df1db0cd94c05e4fe1f537
camtaylor <camerontaylor00@gmail.com>
2016-10-03 22:43:39 -0700

    part a

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ var http = require('http')
 
 
 http.createServer(function(req, res){
-    fs.readFile('public/index.html', 'utf8', function (err, data) {
-        if (err) { throw err;}
         res.writeHead(  200,  {  'Content-Type' : 'text/html'  } );
-        res.write(data);
+        res.write(fs.readFileSync('public/index.html', 'utf-8'));
         res.end(); 
-    });
 
 }).listen(8080);
